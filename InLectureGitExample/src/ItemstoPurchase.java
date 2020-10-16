88300742976f837077ad6d29aed3c2e7f4257829
danielxu <danielxu@localhost>
2020-10-15 22:57:46 -0700

    Added all instance var and getters and setters

--- InLectureGitExample/src/ItemstoPurchase.java
+++ InLectureGitExample/src/ItemstoPurchase.java
@@ -1,15 +1,29 @@
 
 public class ItemstoPurchase {
-	
-//	Private fields
-//	String itemName 
-//	int itemPrice 
-//	int itemQuantity 
-	
-//	Default constructor
+
 	
 	String itemName;
 	int itemPrice;
 	int itemQuantity;
-
+	
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+	
+	
 }
